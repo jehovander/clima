1bf56417947adab56f8807278d967484f31e2395
tu nombre <tu@email.com>
2022-10-21 01:46:00 -0400

    practicando clima

---
+++ src/components/Spinners.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../assets/css/Spinners.css'
+
+
+
+
+const Spinners= () =>{
+    return (
+        <div class="lds-ripple"><div></div><div></div></div>
+    )
+}
+
+
+
+
+export default Spinners;
